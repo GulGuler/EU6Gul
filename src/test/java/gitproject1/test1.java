4345d2307766a9f59b92d7c233ff3b0f05abfa46
GulGuler <rosesmiley80@ymail.com>
2021-09-11 20:19:55 +0100

    working

---
+++ src/test/java/gitproject1/test1.java
@@ -0,0 +1,7 @@
+package gitproject1;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("my first live project with Git");
+    }
+}
---
+++ src/test/java/gitproject1/test2.java
@@ -0,0 +1,8 @@
+package gitproject1;
+
+public class test2 {
+
+    public static void main(String[] args) {
+        System.out.println("I made new additions");
+    }
+}
